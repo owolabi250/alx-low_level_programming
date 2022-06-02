91c6a621484fc34ec455c6e81e0af1978889b6f1
owolabi250 <owolabikehinde250@gmail.com>
2022-06-02 11:51:27 -0700

    good

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#!/bin/bash
 #include<stdio.h>
 /**
-* main - program prints with function puts*
+* main - program prints with function puts
 *
 *Return: Always 0
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle");
 return (0);
 }
